df40bc77b917608f49a7b53f401fba5d9d6a0aa7
Renan Oliveira <56898065+renanspdeoliveira@users.noreply.github.com>
2024-11-11 13:31:22 -0300

    editfooter

--- src/Componentes/Rodape/index.js
+++ src/Componentes/Rodape/index.js
@@ -6,7 +6,7 @@ export const Rodape = () => {
             <section>
            
                 <img className='footer-background'  src='https://raw.githubusercontent.com/renanspdeoliveira/Organo-Alura/refs/heads/main/public/Imagens/fundo.png'></img>
-                <h1 >Desenvolvido por Renan Oliveira.</h1>
+              
             </section>
         </footer>
     )
