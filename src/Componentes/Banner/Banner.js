49f4e7b05b0bc5daeed19caf4c5ba7646cd4ff42
Renan Oliveira <56898065+renanspdeoliveira@users.noreply.github.com>
2024-11-11 13:04:46 -0300

    editimagebackground

--- src/Componentes/Banner/Banner.js
+++ src/Componentes/Banner/Banner.js
@@ -5,7 +5,8 @@ export const Banner = () => {
     return (
         <>
             <header className='banner'>
-                <img className="img-banner" src='./images/banner.png' alt='menu'></img>
+                <img className="img-banner" src='https://raw.githubusercontent.com/renanspdeoliveira/ORGANOJS-ALURA/main/public/Images/banner.png
+' alt='menu'></img>
             </header>
         </>
     )
